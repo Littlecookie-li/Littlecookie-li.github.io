a2646409c55a2729b730ef8644a641722623011c
Littlecookie-li <1522951290@qq.com>
2021-04-22 09:58:58 +0800

    Site updated: 2021-04-22 09:58:57

--- photos/ins.js
+++ photos/ins.js
@@ -116,8 +116,8 @@
           var data = res.list[j].arr;
           var liTmpl = "";
           for (var i = 0, len = data.link.length; i < len; i++) {
-            var minSrc = 'https://github.com/Littlecookie-li/Littlecookie-li.github.io/tree/master/photos/min_index/' + data.link[i];
-            var src = 'https://github.com/Littlecookie-li/Littlecookie-li.github.io/tree/master/photos/index/' + data.link[i];
+            var minSrc = 'https://github.com/Littlecookie-li/Littlecookie-li.github.io/tree/master/photos/min_photos/' + data.link[i];
+            var src = 'https://github.com/Littlecookie-li/Littlecookie-li.github.io/tree/master/photos/photos/' + data.link[i];
             var type = data.type[i];
             var target = src + (type === 'video' ? '.mp4' : '.jpg');
             src += '';
